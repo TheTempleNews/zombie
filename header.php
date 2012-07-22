16cfe31caf25f04285e543213883e7f14328d242
TTN Web Editor <web@temple-news.com>
2012-07-22 09:58:24 -0700

    add google analytics

--- header.php
+++ header.php
@@ -31,6 +31,7 @@
 		<?php wp_head(); ?>
 		<!-- end of wordpress head -->
 		
+		<meta name="google-site-verification" content="ZnmdPLNGk7fPoSfVoEufiCekUIyd7_2nqvYTpsxQVj0" />
 		<!-- advertisement fnord -->
 		<script type='text/javascript'>
 		var googletag = googletag || {};
@@ -61,7 +62,21 @@
 		</script>
 		<!-- end advertisement -->
 		
-		<!-- drop Google Analytics Here -->		
+		<!-- drop Google Analytics Here -->	
+		<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-25802046-1']);
+  _gaq.push(['_setDomainName', 'temple-news.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>	
 		<!-- end analytics -->
 		
 	</head>
