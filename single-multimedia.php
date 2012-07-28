91f4d6f174719f20bb6a6ef948118a7e1103ac95
Chris Montgomery <mont.chr@gmail.com>
2012-07-28 12:26:47 -0400

    clean up single-multimedia styles. most of the styles in the stylesheet for this is either unnecessary or has no effect. the general single post styles have reign here too. enclose the media-dek in <p> tags in attempt to maintain the size of the body element, but this doesn't seem to work… investigate…

--- single-multimedia.php
+++ single-multimedia.php
@@ -24,7 +24,7 @@
 									<h1 class="single-title" itemprop="headline"><?php the_title(); ?></h1>
 									
 									<section class="dek">
-										<?php echo get_post_meta($post->ID, 'media_dek', true); ?>
+										<p><?php echo get_post_meta($post->ID, 'media_dek', true); ?></p>
 									</section> <!-- end dek -->
 									
 									<p class="byline"><i><?php _e("by", "zombietheme"); ?></i> <span class="authors"><?php if(function_exists('coauthors')) coauthors(); else the_author(); ?></span> <time class="sc" datetime="<?php echo the_time('c'); ?>" pubdate><?php echo ttn_article_published_link(); ?></time>
