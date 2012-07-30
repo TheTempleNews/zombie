a381ae2ee64c1818baeaf9c75a265bb364672229
Chris Montgomery <mont.chr@gmail.com>
2012-07-30 13:37:30 -0400

    make hoz layout happen

--- library/zombie-loop.php
+++ library/zombie-loop.php
@@ -132,51 +132,28 @@ function the_zombie_loop( $ttn_section, $show_posts = 5 ) {
 					)
 				);
 				
-								?>
-			
-				
-				<div id="multimedia-mgallery" class="mgallery twelvecol first last clearfix">
-				
-					<div id="post-type-loop-top" class="multimedia-mgallery-top fourcol first clearfix">
-			
-						<?php // begin the loop
-						// ZOMBIE SWARM!
-						if ( $query -> have_posts() ) : while ( $query -> have_posts() ) : $query -> the_post();
+				if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();
 
-					
-						if ( $firstpost == 'firstpost' ) { ?>
+					// this is the second part of the operation that determines first or last class based on column divisions. see above.
+					$k = $style_classes[$style_index++ % $styles_count];
+					?>
+					<article id="post-<?php the_ID(); ?>" <?php post_class( $top_article_class . ' twocol clearfix ' . $k ); ?> role="article">
+							<?php if ( has_post_thumbnail() ) { ?>
+								<div class="featured-image-container">
+									<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('zom-thumb-96'); ?></a>
+								</div>
+							<?php } ?>
+						<header>
+							<div class="post-category-list-container"><?php // the_category_but( $cat_id ); ?></div>
+							<h2 class="headline"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+						</header>
+						<section class="dek">
+							<?php the_excerpt(); ?>
+						</section> <!-- end dek -->
+					</article> <!-- end article -->
+				<?php
 				
-							<?php $firstpost = ''; ?>
-				
-							<article id="post-<?php the_ID(); ?>" <?php post_class( $top_article_class . 'twelvecol first last clearfix' ); ?> role="article">
-								
-								
-								<?php if ( has_post_thumbnail() ) : ?>
-									<div class="featured-image-container featured-image-container-full twelvecol first last">
-										<?php the_post_thumbnail('zom-landscape-576'); ?>
-										</div>
-								<?php endif; ?>
-
-								<header class="clearfix">
-									<!-- <div class="post-category-list-container"><?php // the_category_but( $cat_id ); ?></div> -->
-									<h2 class="home-multimedia-headline multimedia-headline home-multimedia-top-headline multimedia-top-headline headline top-headline"><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-									<p class="home-multimedia-byline byline"><i><?php _e("by", "zombietheme"); ?></i> <span class="home-multimedia-authors multimeda-authors authors"><?php if(function_exists('coauthors_posts_links')) coauthors_posts_links(); else the_author_posts_link(); ?></span> <time class="sc" datetime="<?php echo the_time('c'); ?>" pubdate><?php echo get_the_time( 'd F Y' ) ?></time>
-								</header>
-
-								<section class="dek">
-									<?php the_excerpt(); ?>
-								</section> <!-- end multimedia-dek -->
-
-							</article> <!-- end article -->
-
-						<?php } // end first post
-				
-						endwhile; endif; // close loop
-				
-						rewind_posts(); // start the loop over again ?>
-					
-					</div> <!-- end #post-type-loop-top -->
-				
+				endwhile; endif; // kill loop
 				
 		} // end opinion section
 		
