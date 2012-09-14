f8344349fc30ff0b877fb7f5961fb4403343a35d
Web Editor <web@temple-news.com>
2012-09-14 12:15:53 -0400

    remove analytics code from header - this is provided by yoast's plugin

--- header.php
+++ header.php
@@ -66,17 +66,7 @@
 		<!-- end advertisement -->
 		
 		<!-- drop Google Analytics Here -->	
-		<script type="text/javascript">
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-25802046-1']);
-		  _gaq.push(['_setDomainName', 'temple-news.com']);
-		  _gaq.push(['_trackPageview']);
-		  (function() {
-		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
-		</script>	
+			<!-- or use Yoast's Google Analytics for WordPress plugin @ http://yoast.com/wordpress/google-analytics/ -->
 		<!-- end analytics -->
 		
 	</head>
