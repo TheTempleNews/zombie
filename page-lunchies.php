23e070e2f93ef28631b62fc2ca2b9432c895a940
Web Editor <web@temple-news.com>
2012-09-23 21:45:58 -0400

    add page-lunchies.php

---
+++ page-lunchies.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Template Name: Lunchies
+*/
+
+$year = date('Y');
+
+?>
+
+<?php get_header(); ?>
+			
+			<div id="content">
+			
+				<div id="inner-content" class="wrap clearfix">
+				
+					<div class="lunchies-top-promo">
+				    	<img src="<?php echo get_stylesheet_directory_uri(); ?>/library/images/banners/lunchies/lunchies-banner-<?php echo $year; ?>.png" alt="Lunchies <?php echo $year; ?>" />
+					</div>
+					
+					
+					
+					
+					<div class="lunchies-main first eightcol">
+						
+						
+						
+					</div>
+					
+					
+					
+					
+					<div class="lunchies-sidebar last fourcol">
+						
+					
+					
+					</div>
+				    
+				</div> <!-- end #inner-content -->
+    
+			</div> <!-- end #content -->
+
+<?php get_footer(); ?>
