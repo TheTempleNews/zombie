0585cb9b52672690b0639641f91b797e06d853cb
Web Editor <web@temple-news.com>
2012-09-24 11:57:41 -0400

    loop for editors' rankings

--- page-lunchies.php
+++ page-lunchies.php
@@ -23,13 +23,6 @@ Template Name: Lunchies
 						
 							<?php // The Map
 						
-							$args = array(
-								'post_type'     => 'food_vendor',
-								'category_name' => 'voters-ranking',
-								'year'          => LUNCHIES_YEAR,
-								'posts_per_page' => 10
-							);
-						
 							$map_args = array(
 								'map_post_type'           => 'food_vendor',
 								'map_content'             => 'global',
@@ -40,25 +33,10 @@ Template Name: Lunchies
 								'postal_code'             => array(19122,19121)
 							);
 						
-							$lunchies_rank_voters_query = new WP_Query($args);
-						
 							echo GeoMashup::map($map_args);
 						
-						
-						
-							/* while ( $lunchies_rank_voters_query->have_posts() ) : $lunchies_rank_voters_query->the_post();
-						
-						
-							the_title();
-						
-							endwhile; */
-						
-							//foreach($lunchies_rank_voters_query as $post) : setup_postdata($post); ?>
-						
-						
-						
-							<?php //endforeach; ?>
-						
+							?>
+							
 						</div> <!-- /.lunchies-map -->
 						
 						
@@ -71,22 +49,41 @@ Template Name: Lunchies
 					<div class="lunchies-sidebar last fourcol">
 						
 						<ul class="rank-eds-list">
+						
+						<?php // Vendors: Editors' Rankings
+						
+							$args = array(
+								'post_type'      => 'food_vendor',
+								'category_name'  => 'editors-ranking',
+								'year'           => LUNCHIES_YEAR,
+								'posts_per_page' => 10,
+								'meta_key'       => 'lunchies_rank_eds',
+								'orderby'        => 'meta_value_num',
+								'order'          => 'ASC'
+							);
+							
+							$lunchies_rank_voters_query = get_posts($args);
+					
+							foreach($lunchies_rank_voters_query as $post) : setup_postdata($post); ?>
+					
+						
 							<li class="rank-eds-item">
-								
 								<div class="rank-eds-item-head">
 									<div class="rank-eds-item-outer-circle">
 										<div class="rank-eds-item-inner-circle">
-											<span class="rank-eds-item-rank">1</span>
+											<span class="rank-eds-item-rank"><?php echo get_post_meta( $post->ID, 'lunchies_rank_eds', true ) ?></span>
 										</div>
 									</div>
-									<h3 class="rank-eds-item-title">Sexy Green Truck</h3>
+									<h3 class="rank-eds-item-title"><?php the_title(); ?></h3>
 								</div>
 								
 								<div class="rank-eds-item-img">
-									<img src="http://cambelt.co/360x240" />
+									<?php the_post_thumbnail('zom-landscape-396')?>
 								</div>
-								
 							</li>
+					
+							<?php endforeach; ?>
+						
 						</ul>
 					
 					</div> <!-- /.lunchies-sidebar -->
