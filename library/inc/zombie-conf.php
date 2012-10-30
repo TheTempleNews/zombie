5604998cf086415d274f378cbec271f3c0cb301d
Chris Montgomery <web@temple-news.com>
2012-10-30 02:28:42 -0400

    look ma i can phpdoc sorta

--- library/inc/zombie-conf.php
+++ library/inc/zombie-conf.php
@@ -1,14 +1,22 @@
 <?php
-/* ZOMBIE CONFIUGURATION
+/**
+ * Zombie Theme Configuration File.
+ *
  * Hello. This file contains configuration options
  * that would normally be set on a user-friendly
  * options page. Sorry to make you do this for now.
  * This should be updated in a future version of Zombie.
+ *
+ * @package Zombie/Configuration
+ * @since 1.3
+ *
  */
 
-//* Top Banner Settings *//
+/* Top Banner Settings */
 	// Set this to true to display a banner at the top of the home page
 	define( 'HOME_TOP_PROMO', false );
 
 	// Set this to true to display Lunchies banner. HOME_TOP_BANNER must be set to true.
 	define( 'NEW_LUNCHIES', false );
+	
+	
