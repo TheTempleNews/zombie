014efad41d7a93dcc9f251e32b493117dd6ce64f
Chris Montgomery <web@temple-news.com>
2012-10-30 23:53:15 -0400

    add post template Flipboard Grid

---
+++ special-flipboard.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Template Name: Flipboard Grid
+Post Template: Flipboard Grid
+*/
+?>
+
+<?php
+/**
+ * A Flipboard-inspired layout for articles in a category.
+ * 
+ * @package Zombie\Layouts
+ * @subpackage Special Issue Layouts
+ */
+
+?>
+
+
+
+<?php get_header(); ?>
+			
+			<div id="content">
+			
+				<div id="inner-content" class="wrap clearfix">
+				
+					
+				    
+				</div> <!-- end #inner-content -->
+    
+			</div> <!-- end #content -->
+
+<?php get_footer(); ?>
