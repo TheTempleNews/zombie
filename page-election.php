5edb4fd5e72bbfd3e111d14883329790075da8e4
Chris Montgomery <web@temple-news.com>
2012-11-01 04:57:00 -0400

    create page-election.php

---
+++ page-election.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Template Name: Election
+*/
+?>
+
+<?php get_header(); ?>
+			
+			<div id="content">
+			
+				<div id="inner-content" class="wrap clearfix">
+				
+					
+				    
+				</div> <!-- end #inner-content -->
+    
+			</div> <!-- end #content -->
+
+<?php get_footer(); ?>
