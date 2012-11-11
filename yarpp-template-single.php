810ea1510bdbb4ddc1a24dad6d1a28d2a1f71071
Web Editor <web@temple-news.com>
2012-11-11 14:15:52 -0500

    remove post thumbnail from related posts list; formatting

--- yarpp-template-single.php
+++ yarpp-template-single.php
@@ -7,15 +7,12 @@ Author: montchr / mitcho
 <?php if (have_posts()):?>
 <ol>
 	<?php while (have_posts()) : the_post(); ?>
-		<?php if (has_post_thumbnail()):?>
-			<li><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail(); ?></a></li>
-		<?php endif; ?>
 		<li><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a><!-- (<?php the_score(); ?>)--></li>
 	<?php endwhile; ?>
 </ol>
 
 <?php else:
-query_posts("orderby=rand&order=asc&limit=1");
-the_post();?>
-<p>No related posts were found, so here's a consolation prize: <a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a>.</p>
+	query_posts("orderby=rand&order=asc&limit=1");
+	the_post();?>
+	<p>No related posts were found, so here's a consolation prize: <a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a>.</p>
 <?php endif; ?>
