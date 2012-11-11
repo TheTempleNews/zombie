39a35350c22353501110f4c66d9748bb31b39373
Web Editor <web@temple-news.com>
2012-11-11 14:00:24 -0500

    RELATED POSTS ARE HERE! where's the champagne?

---
+++ yarpp-template-single.php
@@ -0,0 +1,21 @@
+<?php /*
+YARRP Template: Single Random
+This template gives you some random other post in case there are no related posts
+Author: montchr / mitcho
+*/ ?>
+<h3>Related Posts</h3>
+<?php if (have_posts()):?>
+<ol>
+	<?php while (have_posts()) : the_post(); ?>
+		<?php if (has_post_thumbnail()):?>
+			<li><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail(); ?></a></li>
+		<?php endif; ?>
+		<li><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a><!-- (<?php the_score(); ?>)--></li>
+	<?php endwhile; ?>
+</ol>
+
+<?php else:
+query_posts("orderby=rand&order=asc&limit=1");
+the_post();?>
+<p>No related posts were found, so here's a consolation prize: <a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a>.</p>
+<?php endif; ?>
