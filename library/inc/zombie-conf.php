8b2ed2ef344e9b690c7779aa58d7d363fd4433ab
Chris Montgomery <web@temple-news.com>
2012-11-25 13:36:56 -0500

    the stylesheet urls must change with each update. now update ver number manually in inc/zombie-conf.php to match git tag. MUST ANTICIPATE NEXT TAG.

--- library/inc/zombie-conf.php
+++ library/inc/zombie-conf.php
@@ -12,6 +12,9 @@
  *
  */
 
+/* Version Number */
+	define( 'ZOM_VERSION', '1.3.10' );
+
 /* Top Banner Settings */
 	// Set this to true to display a banner at the top of the home page
 	define( 'HOME_TOP_PROMO', false );
