da84e888f1707bf7980cee2c9b0ac8aaffcf7d41
montchr <web@temple-news.com>
2013-03-13 14:31:13 -0400

    bump version to 1.4.3

--- library/inc/zombie-conf.php
+++ library/inc/zombie-conf.php
@@ -13,7 +13,7 @@
  */
 
 /* Version Number */
-	define( 'ZOM_VERSION', '1.4.2' );
+	define( 'ZOM_VERSION', '1.4.3' );
 
 /* Top Banner Settings */
 	// Set this to true to display a banner at the top of the home page
