46cabd2d0b466a384053c9b2ab5512f84d20ec48
montchr <mont.chr@gmail.com>
2013-04-08 21:33:07 -0400

    print stylesheet. only focusing on single post pages.
    lacks header or footer, but that may be preferable to a mess.

--- single.php
+++ single.php
@@ -122,8 +122,12 @@
 									<!--sharexy code start--><noindex><div id="shr_655191" class="sharexy"><script type='text/javascript'>(function(w) { if (!w.SharexyWidget) { w.SharexyWidget = {};} if (!w.SharexyWidget.Params) { w.SharexyWidget.Params = {}; } w.SharexyWidget.Params['655191'] = {'code_id':'655191','publisher_key':'0','design':'metro','layout_static':'h','type':'st','mode_float':'','size_float':'60','size_static':'32','buzz':'0','services':['facebook', 'twitter', 'stumbleupon', 'reddit', 'google_plus'],'url':'current','allways_show_ads':'0','show_ads_sharing':'0','show_ads_cursor':'0','bg_float':'0','bg_color':'#f1f1f1','labels':'','counters':'0', 'counters_float':'0'} })(window);</script><script type='text/javascript' src='http://shuttle.sharexy.com/LoaderLite.js'></script></div></noindex><!--sharexy code end -->
 							
 								</footer> <!-- end article footer -->
-					
-								<?php comments_template(); // comments should go inside the article element ?>
+
+								<section id="comments-section" class="page-break">
+
+									<?php comments_template(); // comments should go inside the article element ?>
+
+								</section>
 					
 							</article> <!-- end article -->
 					
