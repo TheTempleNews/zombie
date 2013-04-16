04bd8e3d2252a7d81f3663a837c60a4cf4ac9467
montchr <mont.chr@gmail.com>
2013-04-16 06:31:02 -0400

    version bump
    start updating style.css too

--- library/inc/zombie-conf.php
+++ library/inc/zombie-conf.php
@@ -13,7 +13,7 @@
  */
 
 /* Version Number */
-	define( 'ZOM_VERSION', '1.4.5.4' );
+	define( 'ZOM_VERSION', '1.4.6' );
 
 /* Top Banner Settings */
 	// Set this to true to display a banner at the top of the home page
