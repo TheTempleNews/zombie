88e9c4e06c360164f8a0f7c32197c6f4d44fba8a
Chris Montgomery <mont.chr@gmail.com>
2013-04-30 14:54:11 -0400

    add margin to bottom of masonry wall - bump version to 1.4.8.4

--- library/inc/zombie-conf.php
+++ library/inc/zombie-conf.php
@@ -13,7 +13,7 @@
  */
 
 /* Version Number */
-	define( 'ZOM_VERSION', '1.4.8.3' );
+	define( 'ZOM_VERSION', '1.4.8.4' );
 
 /* Top Banner Settings */
 	// Set this to true to display a banner at the top of the home page
