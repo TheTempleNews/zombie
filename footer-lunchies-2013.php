a6233f0f87d9f5c233ef11946b7a55e312290d3a
Chris Montgomery <mont.chr@gmail.com>
2013-09-23 11:31:34 -0400

    `page-lunchies-2013.php` width custom header, footer

---
+++ footer-lunchies-2013.php
@@ -0,0 +1,13 @@
+			<footer class="footer" role="contentinfo">
+
+
+
+			</footer> <!-- end footer -->
+
+		</div> <!-- end #container -->
+
+		<?php wp_footer(); // js scripts are inserted using this function ?>
+
+	</body>
+
+</html> <!-- end page. what a ride! -->
---
+++ page-lunchies-2013.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * The Lunchies 2013 Template
+ *
+ * This template exists outside of the Zombie framework.
+ * It enqueues an SCSS-compiled `lunchies-2013.css`,
+ * foregoing any connection to the visual language of
+ * the rest of temple-news.com.
+ *
+ *
+ */
+
+/**
+ * Require Lunchies 2013 functions.
+ */
+require_once('library/inc/special-issues/lunchies/lunchies-2013-functions.php');
+
+?>
+
+<?php get_header('lunchies-2013'); ?>
+
+<div class="main" id="main" role="main">
+	<div class="main__inner" id="main__inner">
+
+
+
+	</div>
+</div>
+
+<?php get_footer('lunchies-2013'); ?>
