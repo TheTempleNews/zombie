20c56fcde49f1b2d819a480ebc5317344ab213fc
Chris Montgomery <mont.chr@gmail.com>
2013-09-23 10:58:16 -0400

    create `lunchies-2013-functions.php`, dequeue unnecessary scripts/styles

---
+++ library/inc/special-issues/lunchies/lunchies-2013-functions.php
@@ -0,0 +1,16 @@
+<?php
+
+// Dequeue unused scripts and styles
+wp_dequeue_style( 'bones-stylesheet' );
+wp_dequeue_style( 'bones-ie-only' );
+wp_dequeue_style( 'flex-tetrodo-css' );
+wp_dequeue_script( 'bones-js' );
+wp_dequeue_script( 'flex-tetrodo-js' );
+wp_dequeue_script( 'fittext-js' );
+wp_dequeue_script( 'slabtext-js' );
+wp_dequeue_script( 'masonry' );
+
+// Enqueue scripts and styles
+
+
+?>
