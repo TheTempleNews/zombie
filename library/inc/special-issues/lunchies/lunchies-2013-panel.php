6b5f520d18ea2764ff51c741977eeb0e482df146
Chris Montgomery <mont.chr@gmail.com>
2013-09-24 02:39:52 -0400

    read more link to the right

--- library/inc/special-issues/lunchies/lunchies-2013-panel.php
+++ library/inc/special-issues/lunchies/lunchies-2013-panel.php
@@ -4,14 +4,16 @@
 		<div class="panel__featured-image  featured-image">
 			<?php the_post_thumbnail('zom-landscape-792'); ?>
 		</div><!--
+
 	--><?php } ?><!--
+
 	--><header class="panel__header">
 		<h1 class="panel__header__post-title  post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 		<div class="panel__header__dek  dek">
 			<?php the_excerpt(); ?>
 		</div>
-	</header>
+	</header><!--
 
-	<a class="panel__read-more" href="<?php the_permalink(); ?>">Read More</a>
+	--><a class="panel__read-more" href="<?php the_permalink(); ?>">Read More</a>
 
 </article>
