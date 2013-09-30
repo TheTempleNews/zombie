d201e30b3ef5955326b171dc879d5438fec88faf
Chris Montgomery <mont.chr@gmail.com>
2013-09-30 17:55:07 -0400

    print edition parent page php

--- page-print.php
+++ page-print.php
@@ -41,64 +41,32 @@
 
 						<h1 class="single-section-name" class="first last twelvecol">Print Edition</h1>
 
-						<div id="print-edition-main" class="ninecol last">
+						<div id="print-edition-main">
 
 							<?php if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
 
-								<?php $vol_years = '';
-
-								if ( get_post_meta($post->ID, 'print_vol', true) == 90 ) {
-									$vol_years = 2011 . "&ndash;" . 2012;
-								} elseif ( get_post_meta($post->ID, 'print_vol', true) == 91 ) {
-									$vol_years = 2012 . "&ndash;" . 2013;
-								} ?>
-
 								<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
-									<header class="article-header">
-
-										<h2 class="print-edition-title">
-											Volume <?php echo get_post_meta($post->ID, 'print_vol', true); ?><span class="vol-years"> / <?php echo $vol_years; ?></span><br />
-											Issues <?php echo get_post_meta($post->ID, 'print_issueno', true); if ( get_post_meta($post->ID, 'print_semester', true) ) { ?><span class="issues-semester"> / <?php get_post_meta($post->ID, 'print_semester', true) ?></span><?php } //don't remove this! ?>
-										</h2>
-
-									</header> <!-- end article header -->
-
 									<section class="post-content clearfix" itemprop="articleBody">
-										<?php the_content(); ?>
+
+										<?php
+
+										wp_list_pages( array(
+										              'sort_column' => 'menu_order',
+										              'title_li' => '',
+										              'post_type' => 'print_edition',
+										              )
+										);
+
+										?>
+
 									</section> <!-- end article section -->
 
 									<footer class="article-footer">
 
 										<!--sharexy code start--><noindex><div id="shr_655191" class="sharexy"><script type='text/javascript'>(function(w) { if (!w.SharexyWidget) { w.SharexyWidget = {};} if (!w.SharexyWidget.Params) { w.SharexyWidget.Params = {}; } w.SharexyWidget.Params['655191'] = {'code_id':'655191','publisher_key':'0','design':'metro','layout_static':'h','type':'st','mode_float':'','size_float':'60','size_static':'32','buzz':'0','services':['facebook', 'twitter', 'stumbleupon', 'reddit', 'google_plus'],'url':'current','allways_show_ads':'0','show_ads_sharing':'0','show_ads_cursor':'0','bg_float':'0','bg_color':'#f1f1f1','labels':'','counters':'0', 'counters_float':'0'} })(window);</script><script type='text/javascript' src='http://shuttle.sharexy.com/LoaderLite.js'></script></div></noindex><!--sharexy code end -->
 
-
-										<!-- <div id="print-carousel-container" class="clearfix first last clearfix">
-											<h4 class="section-box-title">Recent Issues</h2>
-
-												<div id="print-es-carousel" class="es-carousel-wrapper">
-													<div class="es-carousel">
-														<ul>
-															<li>
-																<a href="#">
-																	<img src="images/medium/1.jpg" alt="image01" />
-																</a>
-															</li>
-														</ul>
-													</div>
-												</div> <!-- end elastislide carousel -->
-
-										<!-- </div> <!-- end #section-box-print-carousel -->
-
-
-
-
-
-										<?php // the_tags('<p class="tags"><span class="tags-title">Tags:</span> ', ', ', '</p>'); ?>
-
 									</footer> <!-- end article footer -->
-
-									<?php comments_template(); // comments should go inside the article element ?>
 
 								</article> <!-- end article -->
 
@@ -121,16 +89,6 @@
 							<?php endif; ?>
 
 						</div> <!-- end #print-edition-main -->
-
-						<aside id="print-edition-aside" class="threecol first">
-
-								<?php wp_list_pages( array(
-										'post_type'       => 'print_edition',
-										'title_li'        => __('Back Issues')
-									)
-								); ?>
-
-						</aside>
 
 					</div> <!-- end #main -->
 
