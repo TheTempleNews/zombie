d137e115e2fd183d86345f169ce2a3e1b1f3f242
Chris Montgomery <mont.chr@gmail.com>
2013-09-30 17:50:25 -0400

    single print edition using breadcrumbs, wp_list_pages

--- single-print_edition.php
+++ single-print_edition.php
@@ -23,14 +23,6 @@
 						$obj = $wp_post_types[$post_type];
 						$post_type_name = $obj->labels->singular_name;
 
-						$vol_years = '';
-
-						if ( get_post_meta($post->ID, 'print_vol', true) == 90 ) {
-							$vol_years = 2011 . "&ndash;" . 2012;
-						} elseif ( get_post_meta($post->ID, 'print_vol', true) == 91 ) {
-							$vol_years = 2012 . "&ndash;" . 2013;
-						}
-
 						?>
 
 			<div id="content">
@@ -39,7 +31,14 @@
 
 					<div id="main" class="eightcol first clearfix" role="main">
 
-						<h1 class="single-section-name" class="first last twelvecol"><?php echo $post_type_name; ?></h1>
+						<h1 class="single-section-name" class="first last twelvecol">
+							<div class="breadcrumbs">
+								<?php if(function_exists('bcn_display'))
+								{
+									bcn_display();
+								}?>
+							</div>
+						</h1>
 
 						<div id="print-edition-main" class="ninecol last">
 
@@ -47,50 +46,38 @@
 
 								<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
-									<header class="article-header">
+									<?php if (get_field('print_type') === 'issue') : ?>
 
-										<h2 class="print-edition-title">
-											Volume <?php echo get_post_meta($post->ID, 'print_vol', true); ?><span class="vol-years"><?php echo " / " . $vol_years; ?></span><br />
-											Issues <?php echo get_post_meta($post->ID, 'print_issueno', true); if ( get_post_meta($post->ID, 'print_semester', true) ) { ?><span class="issues-semester"> / <?php get_post_meta($post->ID, 'print_semester', true) ?></span><?php } //don't remove this! ?>
-										</h2>
+										<header class="article-header">
+											<h2 class="print-edition-title"><?php the_date(); ?></h2>
+										</header> <!-- end article header -->
 
-									</header> <!-- end article header -->
+									<?php endif; ?>
 
 									<section class="post-content clearfix" itemprop="articleBody">
-										<?php the_content(); ?>
+										<?php
+
+										the_content();
+
+										wp_list_pages( array(
+										              'depth' => 1,
+										              'sort_column' => 'menu_order',
+										              'title_li' => '',
+										              'post_type' => 'print_edition',
+										              'child_of' => get_the_ID()
+										              )
+										);
+
+										?>
 									</section> <!-- end article section -->
 
 									<footer class="article-footer">
 
 										<!--sharexy code start--><noindex><div id="shr_655191" class="sharexy"><script type='text/javascript'>(function(w) { if (!w.SharexyWidget) { w.SharexyWidget = {};} if (!w.SharexyWidget.Params) { w.SharexyWidget.Params = {}; } w.SharexyWidget.Params['655191'] = {'code_id':'655191','publisher_key':'0','design':'metro','layout_static':'h','type':'st','mode_float':'','size_float':'60','size_static':'32','buzz':'0','services':['facebook', 'twitter', 'stumbleupon', 'reddit', 'google_plus'],'url':'current','allways_show_ads':'0','show_ads_sharing':'0','show_ads_cursor':'0','bg_float':'0','bg_color':'#f1f1f1','labels':'','counters':'0', 'counters_float':'0'} })(window);</script><script type='text/javascript' src='http://shuttle.sharexy.com/LoaderLite.js'></script></div></noindex><!--sharexy code end -->
 
-
-										<!-- <div id="print-carousel-container" class="clearfix first last clearfix">
-											<h4 class="section-box-title">Recent Issues</h2>
-
-												<div id="print-es-carousel" class="es-carousel-wrapper">
-													<div class="es-carousel">
-														<ul>
-															<li>
-																<a href="#">
-																	<img src="images/medium/1.jpg" alt="image01" />
-																</a>
-															</li>
-														</ul>
-													</div>
-												</div> <!-- end elastislide carousel -->
-
-										<!-- </div> <!-- end #section-box-print-carousel -->
-
-
-
-
-
 										<?php // the_tags('<p class="tags"><span class="tags-title">Tags:</span> ', ', ', '</p>'); ?>
 
 									</footer> <!-- end article footer -->
-
-									<?php comments_template(); // comments should go inside the article element ?>
 
 								</article> <!-- end article -->
 
@@ -99,15 +86,15 @@
 							<?php else : ?>
 
 								<article id="post-not-found" class="hentry clearfix">
-						    		<header class="article-header">
-						    			<h1><?php _e("Oops, Post Not Found!", "zombietheme"); ?></h1>
-						    		</header>
-						    		<section class="post-content">
-						    			<p><?php _e("Uh Oh. Something is missing. Try double checking things.", "zombietheme"); ?></p>
-						    		</section>
-						    		<footer class="article-footer">
-						    		    <p><?php _e("This is the error message in the single.php template.", "zombietheme"); ?></p>
-						    		</footer>
+									<header class="article-header">
+										<h1><?php _e("Oops, Post Not Found!", "zombietheme"); ?></h1>
+									</header>
+									<section class="post-content">
+										<p><?php _e("Uh Oh. Something is missing. Try double checking things.", "zombietheme"); ?></p>
+									</section>
+									<footer class="article-footer">
+										<p><?php _e("This is the error message in the single.php template.", "zombietheme"); ?></p>
+									</footer>
 								</article>
 
 							<?php endif; ?>
@@ -118,7 +105,7 @@
 
 								<?php wp_list_pages( array(
 										'post_type'       => 'print_edition',
-										'title_li'        => __('Back Issues')
+										'title_li'        => __('')
 									)
 								); ?>
 
