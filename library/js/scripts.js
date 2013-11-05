e4a042a45825b5078d6b9adcb889bbbfc81e9526
Chris Montgomery <mont.chr@gmail.com>
2013-11-05 01:46:11 -0500

    bp fittext

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -48,6 +48,7 @@ jQuery(document).ready(function($) {
 	$('.special-issue-banner--lunchies-2013 h2.fittext').fitText();
 	$('.breaking-news-banner.fittext p').fitText(2.25);
 	$('.special-issue-banner--reunion-2013 h2.fittext').fitText();
+	$('.special-issue-banner--basketball-preview-2013 h2.fittext').fitText();
 
 	/* slabText */
 	$(".slabtextthis").slabText();
