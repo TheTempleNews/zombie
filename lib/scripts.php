563c4ae5d6688f83b36ca090d5c7352ef9988897
Chris Montgomery <mont.chr@gmail.com>
2013-11-11 21:24:51 -0500

    run grunt in new v2 env

--- lib/scripts.php
+++ lib/scripts.php
@@ -14,7 +14,7 @@
 function zombie_scripts() {
   // Main stylesheet
   // wp_enqueue_style( 'bones-stylesheet', get_stylesheet_directory_uri() . '/library/css/style.css', array(), ZOM_VERSION, 'all' );
-  wp_enqueue_style('zombie-main', get_stylesheet_directory_uri() . '/assets/css/main.css', array(), 'de894f49196b5232dcfab0b828bb615d');
+  wp_enqueue_style('zombie-main', get_stylesheet_directory_uri() . '/assets/css/main.css', array(), '304bba61685330cb1563168e4255d7bd');
 
   // IE-only stylesheet
   // wp_register_style('zombie-ie-stylesheet', get_stylesheet_directory_uri() . '/assets/css/ie.css', array(), '');
