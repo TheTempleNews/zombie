586609639b9451a52bbf7d7583bd8c363c970171
Chris Montgomery <mont.chr@gmail.com>
2013-12-06 13:32:18 -0500

    bumpversion to 1.5.22, turn off owlery banner [deploy:production]

--- library/inc/zombie-conf.php
+++ library/inc/zombie-conf.php
@@ -13,11 +13,11 @@
  */
 
 /* Version Number */
-	define( 'ZOM_VERSION', '1.5.21' );
+	define( 'ZOM_VERSION', '1.5.22' );
 
 /* Top Banner Settings */
 	// Set this to true to display a banner at the top of the home page
-	define( 'HOME_TOP_PROMO', true );
+	define( 'HOME_TOP_PROMO', false );
 
 	// Set this to true to display a banner at the top of the single article
 	define( 'SINGLE_TOP_PROMO', false );
@@ -57,4 +57,4 @@
 
 	define( 'BREAKING_NEWS', false );
 
-	define( 'THE_OWLERY_BANNER', true );
+	define( 'THE_OWLERY_BANNER', false );
