8ed897075c523946dfe8e69806e2150a7d7c288f
Chris Montgomery <mont.chr@gmail.com>
2014-02-03 20:00:13 -0500

    prep essayist for continual use
    This isn't the ideal solution - Essayist could use some refinements.
    But new essays are coming in and this looks good enough for now.

--- page-essays.php
+++ page-essays.php
@@ -29,94 +29,50 @@ Template Name: The Essayist
 						<?php endwhile; endif; ?>
 
 						<section id="essay-feed" class="twelvecol clearfix">
-
 							<?php
-
-							// set class to first or last depending on position in two column layout
-							// http://wordpress.org/support/topic/adding-different-styling-every-3rd-post
-							$style_classes = array('first', 'last');
-							$styles_count = count($style_classes);
-							$style_index = 0;
-
-							$featured_essays_args = array(
-								'post_type' => 'essay',
-								'meta_query' => array(
-									array(
-										'key' => 'feature',
-										'value' => true,
-										'compare' => '='
-									)
-								),
-								'posts_per_page' => 2
-							);
-
-							$featured_essays = new WP_Query($featured_essays_args);
-
-							if ($featured_essays->have_posts()) : while ($featured_essays->have_posts()) : $featured_essays->the_post();
-
-								// this is the second part of the operation that determines first or last class based on column divisions. see above.
-								$k = $style_classes[$style_index++ % $styles_count];
-
-							?>
-
-								<article id="post-42483" class="sixcol <?php echo $k; ?> clearfix" role="article">
-
-									<header class="article-header">
-
-										<h3 class="headline h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-									
-										<p class="byline"><i>by</i> <span class="authors"><?php if(function_exists('coauthors_posts_links')) coauthors_posts_links(); else the_author_posts_link(); ?></span></p>
-
-									</header> <!-- end article header -->
-							
-									<section class="dek">
-										<?php the_excerpt(); ?>
-									</section> <!-- end .dek -->
-							
-								</article> <!-- end article -->
-
-							<?php endwhile; endif; 
-
-							wp_reset_postdata();
-
-							$is_featured = '';
+							global $wp_query;
+							$temp_query = $wp_query;
+							$wp_query = null;
+							$wp_query = new WP_Query();
+							$essays = $wp_query;
 
 							$essays_args = array(
 								'post_type' => 'essay',
-								//'meta_key' => 'feature',
-								//'meta_value' => false
+								'posts_per_page' => 5,
+								'paged' => $paged
 							);
+							//$essays = new WP_Query($essays_args);
+							$essays->query($essays_args);
 
-							$essays = new WP_Query($essays_args);
-
-							if ($essays->have_posts()) : while ($essays->have_posts()) : $essays->the_post();
-
-								$is_featured = get_post_meta($post->ID, 'feature');
-
-								if ($is_featured) {
-									continue;
-								}
-							?>
-
+							if ($essays->have_posts()) : while ($essays->have_posts()) : $essays->the_post(); ?>
 								<article id="post-<?php the_ID(); ?>" <?php post_class('sixcol push_three clearfix'); ?> role="article">
-
 									<header class="article-header">
-
 										<h3 class="headline h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-									
 										<p class="byline"><i>by</i> <span class="authors"><?php if(function_exists('coauthors_posts_links')) coauthors_posts_links(); else the_author_posts_link(); ?></span></p>
-
 									</header> <!-- end article header -->
-							
-									<section class="dek">
+									<div class="dek">
 										<?php the_excerpt(); ?>
-									</section> <!-- end .dek -->
-							
+									</div> <!-- end .dek -->
 								</article> <!-- end article -->
+							<?php endwhile; endif; ?>
 
-							<?php endwhile; endif; 
+							<div class="essays-pagination sixcol push_three">
+								<?php if (function_exists('bones_page_navi')) { // if experimental feature is active 
+									bones_page_navi(); // use the page navi function
+								} else { // if it is disabled, display regular wp prev & next links ?>
+									<nav class="wp-prev-next">
+										<ul class="clearfix">
+											<li class="prev-link"><?php next_posts_link(_e('&laquo; Older Entries', "zombietheme")) ?></li>
+											<li class="next-link"><?php previous_posts_link(_e('Newer Entries &raquo;', "zombietheme")) ?></li>
+										</ul>
+									</nav>
+								<?php }
 
-							wp_reset_postdata(); ?>
+								$essays = null;
+								$wp_query = null;
+								$wp_query = $temp_query;
+								?>
+							</div>
 
 						</section> <!-- end #essay-feed -->
 
