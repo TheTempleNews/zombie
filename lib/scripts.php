02976cfce63da2ee7db9549f6cba2a4bbc44e3c3
Chris Montgomery <mont.chr@gmail.com>
2014-02-17 21:30:35 -0500

    trailing spaces

--- lib/scripts.php
+++ lib/scripts.php
@@ -14,7 +14,7 @@
 function zombie_scripts() {
   // Main stylesheet
   // wp_enqueue_style( 'bones-stylesheet', get_stylesheet_directory_uri() . '/library/css/style.css', array(), ZOM_VERSION, 'all' );
-  wp_enqueue_style('zombie-main', get_stylesheet_directory_uri() . '/assets/css/main.css', array(), '3affb869eb765158f906d1ef2685b798');
+  wp_enqueue_style('zombie-main', get_stylesheet_directory_uri() . '/assets/css/main.css', array(), '7b924de5712db37ea43a2746c2fab905');
 
   // IE-only stylesheet
   // wp_register_style('zombie-ie-stylesheet', get_stylesheet_directory_uri() . '/assets/css/ie.css', array(), '');
