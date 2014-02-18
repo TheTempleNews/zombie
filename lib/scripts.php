5b263eae5edd3fe270113960b68132a8bb9b9ec0
Chris Montgomery <mont.chr@gmail.com>
2014-02-18 00:50:32 -0500

    remove bottom margin on site tagline

--- lib/scripts.php
+++ lib/scripts.php
@@ -14,7 +14,7 @@
 function zombie_scripts() {
   // Main stylesheet
   // wp_enqueue_style( 'bones-stylesheet', get_stylesheet_directory_uri() . '/library/css/style.css', array(), ZOM_VERSION, 'all' );
-  wp_enqueue_style('zombie-main', get_stylesheet_directory_uri() . '/assets/css/main.css', array(), '2da0f7f7cd73a2c38fc3e1dc35372c19');
+  wp_enqueue_style('zombie-main', get_stylesheet_directory_uri() . '/assets/css/main.css', array(), '357467365fda7511351de9293982e527');
 
   // IE-only stylesheet
   // wp_register_style('zombie-ie-stylesheet', get_stylesheet_directory_uri() . '/assets/css/ie.css', array(), '');
