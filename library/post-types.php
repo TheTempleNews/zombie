97f991a0bdb6f11012e06283c400fe2958b99e6e
Chris Montgomery <mont.chr@gmail.com>
2014-02-19 20:46:55 -0500

    re-add banner cpt after it was removed
    It mysteriously went missing about the same time that the post type
    files were last changed in 305292b5...

--- library/post-types.php
+++ library/post-types.php
@@ -1,7 +1,5 @@
 <?php
 
-// Hook into the 'init' action
-add_action( 'init', 'zombie_register_custom_post_types', 0 );
 
 function zombie_register_custom_post_types() {
 
@@ -560,4 +558,49 @@ function zombie_register_custom_post_types() {
                      )
   );
 
-}
+
+  /**
+   * Banners
+   */
+  $banner_labels = array(
+      'name'                => 'Banners',
+      'singular_name'       => 'Banner',
+      'menu_name'           => 'Banners',
+      'parent_item_colon'   => 'Parent Banner:',
+      'all_items'           => 'All Banners',
+      'view_item'           => 'View Banner',
+      'add_new_item'        => 'Add New Banner',
+      'add_new'             => 'New Banner',
+      'edit_item'           => 'Edit Banner',
+      'update_item'         => 'Update Banner',
+      'search_items'        => 'Search banners',
+      'not_found'           => 'No banners found',
+      'not_found_in_trash'  => 'No banners found in Trash',
+  );
+  $banner_args = array(
+      'label'               => 'banner',
+      'description'         => 'Top-of-site banners, including breaking news and promotions.',
+      'labels'              => $banner_labels,
+      'supports'            => array( 'title', 'editor', 'thumbnail', 'custom-fields', ),
+      'taxonomies'          => array( 'category', 'post_tag' ),
+      'hierarchical'        => false,
+      'public'              => true,
+      'show_ui'             => true,
+      'show_in_menu'        => true,
+      'show_in_nav_menus'   => true,
+      'show_in_admin_bar'   => true,
+      'menu_position'       => 5,
+      'menu_icon'           => '',
+      'can_export'          => true,
+      'has_archive'         => true,
+      'exclude_from_search' => true,
+      'publicly_queryable'  => true,
+      'capability_type'     => 'page',
+  );
+  register_post_type( 'banner', $banner_args );
+
+} // don't touch this
+
+
+// Hook into the 'init' action
+add_action( 'init', 'zombie_register_custom_post_types', 0 );
